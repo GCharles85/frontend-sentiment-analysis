7f0c450960abbf4d6a1e01cabeff288daab35f7e
GCharles85 <gcharles345@gmail.com>
2023-04-13 12:38:02 -0400

    Sentiment Analysis on Frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SentimentAnalysisComponent } from './sentiment-analysis/sentiment-analysis.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SentimentAnalysisComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sentiment-analysis/sentiment-analysis.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import * as Sentiment from 'sentiment'; // using the AFINN-165 library
+
+@Component({
+  selector: 'app-sentiment-analysis',
+  templateUrl: './sentiment-analysis.component.html',
+  styleUrls: ['./sentiment-analysis.component.css']
+})
+export class SentimentAnalysisComponent {
+  analysisResult: any;
+
+  performAnalysis() {
+    const sentiment = new Sentiment();
+    const userInput = window.prompt('Enter some text:');
+    if (userInput !== null) {
+      this.analysisResult = sentiment.analyze(userInput);
+    }
+  }
+}
